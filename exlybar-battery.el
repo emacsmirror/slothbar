0222b866373cb89f240845f90fcf552418b8fac0
Fran Ley <fran.ley@mailfence.com>
2025-06-01 01:58:00 -0700

    ; 0.25.1 -> 0.25.2

--- exlybar-battery.el
+++ exlybar-battery.el
@@ -4,7 +4,7 @@
 
 ;; Author: Jo Gay <jo.gay@mailfence.com>
 ;;      Fran Ley <fran.ley@mailfence.com>
-;; Version: 0.25.1
+;; Version: 0.25.2
 ;; Package-Requires: ((cl-lib "0.5") (emacs "27.1"))
 ;; Keywords: window-manager, status-bar, exwm
 
