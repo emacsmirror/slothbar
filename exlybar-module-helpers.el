55348eab2289967908340512ed075a1c4836d1fd
Fran Ley <fran.ley@mailfence.com>
2025-06-08 21:09:22 -0700

    ; 0.27.0 -> 0.27.5
    - bugfix for exlybar-workspaces
    - bugfixes for randr struts and refresh
    
    - herbstluftwm helpers for exlybar-workspaces
    - xmonad helpers for exlybar-workspaces

--- exlybar-module-helpers.el
+++ exlybar-module-helpers.el
@@ -3,7 +3,7 @@
 ;; Copyright (C) 2021 Jo Gay <jo.gay@mailfence.com>
 
 ;; Author: Jo Gay <jo.gay@mailfence.com>
-;; Version: 0.27.0
+;; Version: 0.27.5
 ;; Package-Requires: ((emacs "27.1"))
 ;; Keywords: window-manager, status-bar, exwm
 
