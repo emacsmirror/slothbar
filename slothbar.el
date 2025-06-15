86e2fd3526d76f08fbc6da153eb7c00dcdce0d2f
Agnes Li <agnes.li@mailfence.com>
2025-06-15 10:54:08 -0700

    ; Try to improve documentation about font configuration

--- slothbar.el
+++ slothbar.el
@@ -61,6 +61,29 @@
 ;; To exit:
 ;; M-x: slothbar-exit
 
+;; On first run, expect a delay of around twenty to thirty seconds
+;; (more or less depending on which fonts are chosen). During this
+;; time, available fonts configured in slothbar-font-candidates are
+;; loaded and cached in a background process for future reference.
+
+;; Check the default value of slothbar-font-candidates in
+;; slothbar-font.el to determine a minimal set of fonts.
+
+;; E.g. if slothbar-font-candidates is:
+;;   '(("Aporetic Sans" "IBM Plex Serif" "Deja Vu Serif" "Cantarell")
+;;     ("Font Awesome")
+;;     ("Aporetic Sans Mono" "IBM Plex Mono" "DejaVu Sans Mono:style=Book")
+;;     ("all-the-icons")
+;;     ("Symbols Nerd Font Mono"))
+;; then for module format strings using:
+;; + ^f0 ensure one of Aporetic Sans, IBM Plex Serif, Deja Vu Serif, or
+;;   Cantarell is installed
+;; + ^f1 install the free version of font awesome
+;; + ^f2 ensure one of Aporetic Sans Mono, IBM Plex Mono, or Deja Vu Sans
+;;   Mono is installed
+;; + ^f3 run M-x all-the-icons-install-fonts (or install another way)
+;; + ^f4 run M-x nerd-icons-install-fonts (or install another way)
+
 ;;; Code:
 
 (require 'cl-lib)
